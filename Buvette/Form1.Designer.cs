a02fea356469bbfe863c33c41d979eb7d028805c
yh20 <yh20@DESKTOP-2I34250>
2021-01-22 23:48:53 +0100

    client

---
+++ Buvette/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Buvette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.p = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.prixT = new System.Windows.Forms.Label();
+            this.categories = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Elements = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Supprimer = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.prixT);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.p);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(1036, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(437, 686);
+            this.panel2.TabIndex = 1;
+            // 
+            // p
+            // 
+            this.p.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.p.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.p.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Elements,
+            this.pr,
+            this.Supprimer});
+            this.p.Location = new System.Drawing.Point(-3, 121);
+            this.p.Name = "p";
+            this.p.RowHeadersWidth = 40;
+            this.p.RowTemplate.Height = 40;
+            this.p.Size = new System.Drawing.Size(437, 411);
+            this.p.TabIndex = 3;
+            this.p.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.p_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(11, 613);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(423, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Commander";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 44);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Panier";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.categories);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1036, 686);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 558);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Prix Totale ";
+            // 
+            // prixT
+            // 
+            this.prixT.AutoSize = true;
+            this.prixT.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prixT.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.prixT.Location = new System.Drawing.Point(157, 558);
+            this.prixT.Name = "prixT";
+            this.prixT.Size = new System.Drawing.Size(62, 28);
+            this.prixT.TabIndex = 5;
+            this.prixT.Text = "0 Dh";
+            // 
+            // categories
+            // 
+            this.categories.AutoSize = true;
+            this.categories.Dock = System.Windows.Forms.DockStyle.Top;
+            this.categories.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categories.ForeColor = System.Drawing.Color.White;
+            this.categories.Location = new System.Drawing.Point(0, 0);
+            this.categories.Name = "categories";
+            this.categories.Size = new System.Drawing.Size(1036, 37);
+            this.categories.TabIndex = 5;
+            this.categories.TabStop = false;
+            this.categories.Text = "CHOISIR UNE CATEGORIE";
+            this.categories.Enter += new System.EventHandler(this.categories_Enter);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(0, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1036, 649);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SOUS CATEGORIES";
+            // 
+            // Elements
+            // 
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Elements.DefaultCellStyle = dataGridViewCellStyle19;
+            this.Elements.FillWeight = 99.99998F;
+            this.Elements.HeaderText = "Elements";
+            this.Elements.Name = "Elements";
+            this.Elements.ReadOnly = true;
+            // 
+            // pr
+            // 
+            dataGridViewCellStyle20.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold);
+            this.pr.DefaultCellStyle = dataGridViewCellStyle20;
+            this.pr.HeaderText = "Prix";
+            this.pr.Name = "pr";
+            this.pr.ReadOnly = true;
+            // 
+            // Supprimer
+            // 
+            dataGridViewCellStyle21.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle21.BackColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle21.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle21.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle21.SelectionBackColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle21.SelectionForeColor = System.Drawing.Color.White;
+            this.Supprimer.DefaultCellStyle = dataGridViewCellStyle21;
+            this.Supprimer.FillWeight = 99.99998F;
+            this.Supprimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Supprimer.HeaderText = "Supprimer";
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Supprimer.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Supprimer.Text = "Supprimer";
+            this.Supprimer.UseColumnTextForButtonValue = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1473, 686);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Buvette";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView p;
+        private System.Windows.Forms.Label prixT;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox categories;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Elements;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pr;
+        private System.Windows.Forms.DataGridViewButtonColumn Supprimer;
+    }
+}
+
---
+++ Buvette/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Buvette
+{
+    public partial class Form1 : Form
+    {
+        DataClasses1DataContext co = new DataClasses1DataContext();
+        List<String> panier = new List<String>();
+        double prix_totale;
+
+        public Form1()
+        {
+            InitializeComponent();
+            var produits = from c in co.Produit select c.nom;
+            var i = 0;
+            var j = 0;
+            foreach (var obj in produits)
+            {
+                if (j == 3) { j = 0; i++; }
+                Button Mybutton = new Button();
+                Mybutton.Location = new Point(250 * j + 150, 150 * i + 50);
+                Mybutton.Size = new Size(200, 100);
+                Mybutton.Text = obj;
+                Mybutton.TextAlign = ContentAlignment.MiddleCenter;
+                Mybutton.FlatStyle = FlatStyle.Flat;
+                Mybutton.BackColor = Color.CornflowerBlue;
+                Mybutton.ForeColor = Color.White;
+                Mybutton.Font = new Font("Cambria", 18);
+                Mybutton.Click += selectcategories;
+                // Adding this button to form 
+                categories.Controls.Add(Mybutton);
+                j++;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectcategories(object sender, EventArgs e)
+        {
+            groupBox1.Controls.Clear();
+            Button produit = sender as Button;
+
+            groupBox1.Text = produit.Text;
+
+            var y = (from h in co.Produit
+                     join d in co.Sous_Produit
+                        on h.ref_produit equals d.ref_produit
+                     where h.nom == produit.Text
+                     select new { d.nom , d.prix_unitaire });
+            var i = 0;
+            var j = 0;
+            foreach (var obj in y)
+            {
+                if (j == 3) { j = 0; i++; }
+                Button Mybutton = new Button();
+                Mybutton.Location = new Point(250 * j + 150, 150 * i + 100);
+                Mybutton.Size = new Size(165, 165);
+                Mybutton.Text = obj.nom +'\n' +obj.prix_unitaire +"Dh";
+                Mybutton.FlatStyle = FlatStyle.Flat;
+                Mybutton.BackColor = Color.CornflowerBlue;
+                Mybutton.ForeColor = Color.White;
+                Mybutton.Padding = new Padding(20);
+                Mybutton.Font = new Font("Cambria", 18);
+                Mybutton.Click += addPanier;
+                // Adding this button to form 
+                groupBox1.Controls.Add(Mybutton);
+                j++;
+
+            }
+        }
+        public void addPanier(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            var StringPrix = b.Text.Substring(b.Text.IndexOf('\n') + 1);
+            double prix = Convert.ToDouble(StringPrix.Substring(0, StringPrix.Length - 2));
+            p.Rows.Add(b.Text.Substring(0, b.Text.IndexOf('\n')),prix);
+
+            prix_totale = 0;
+            for (int i = 0; i < p.Rows.Count; ++i)
+
+            {
+
+                prix_totale += Convert.ToDouble(p.Rows[i].Cells[1].Value);
+
+            }
+            prixT.Text = prix_totale + " Dh";
+
+        }
+
+        private void p_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(p.Columns[e.ColumnIndex].Name == "Supprimer" && e.RowIndex != p.Rows.Count-1)
+            {
+                prix_totale -= Convert.ToDouble(p.Rows[e.RowIndex].Cells[1].Value);
+                p.Rows.RemoveAt(e.RowIndex);
+                prixT.Text = prix_totale + " Dh";
+
+            }
+        }
+
+        private void categories_Enter(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
